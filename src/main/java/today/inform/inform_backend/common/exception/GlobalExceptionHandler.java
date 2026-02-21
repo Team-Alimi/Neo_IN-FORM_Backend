be9064282b0af2c6b026f43a5268c8cc7a1cbd46
IM5K58 <gitue11@gmail.com>
2026-02-22 01:51:27 +0900

    feat: 에러코드 뜨게 설정

--- src/main/java/today/inform/inform_backend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/today/inform/inform_backend/common/exception/GlobalExceptionHandler.java
@@ -4,8 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import today.inform.inform_backend.common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -30,6 +31,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+        log.error("Unhandled Exception caught in GlobalExceptionHandler: ", e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.fail("INTERNAL_SERVER_ERROR", "서버 내부 오류가 발생했습니다."));
--- src/main/java/today/inform/inform_backend/repository/CategoryRepository.java
+++ src/main/java/today/inform/inform_backend/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package today.inform.inform_backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import today.inform.inform_backend.entity.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Optional<Category> findByCategoryName(String categoryName);
 }
